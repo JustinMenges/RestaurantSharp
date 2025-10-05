4e31fb1942b86057e365413991600045aa8fa3bc
JustinMenges <justinmenges@hotmail.com>
2025-10-05 18:05:08 +0200

    Create menuitems controller

---
+++ RestaurantSharp.Presentation/Controllers/MenuItemsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantSharp.Presentation.Controllers;
+
+public class MenuItemsController : ControllerBase
+{
+    public async Task<IActionResult> GetAvailable()
+    {
+
+    }
+}
