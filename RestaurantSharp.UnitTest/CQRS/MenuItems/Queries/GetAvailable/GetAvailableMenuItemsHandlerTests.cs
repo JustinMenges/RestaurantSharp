cf30dc978c7274d796a81f011d46cfe77207670c
Justin Menges <jmenges2@usgpeople.nl>
2025-10-07 16:03:12 +0200

    chore: create a setup for the first unittest

---
+++ RestaurantSharp.UnitTest/CQRS/MenuItems/Queries/GetAvailable/GetAvailableMenuItemsHandlerTests.cs
@@ -0,0 +1,19 @@
+﻿using Moq;
+using RestaurantSharp.Application.Common.Interfaces;
+using RestaurantSharp.Application.CQRS.MenuItems.Queries.GetAvailable;
+
+namespace RestaurantSharp.UnitTest.CQRS.MenuItems.Queries.GetAvailable;
+
+[TestFixture]
+public class GetAvailableMenuItemsHandlerTests
+{
+    private Mock<IMenuItemRepository> _mockRepo;
+    private GetAvailableMenuItemsHandler _handler;
+
+    [SetUp]
+    public void Setup()
+    {
+        _mockRepo = new Mock<IMenuItemRepository>();
+        _handler = new GetAvailableMenuItemsHandler(_mockRepo.Object);
+    }
+}
